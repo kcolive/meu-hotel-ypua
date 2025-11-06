1c18d4ee6439792fc32ae0c2adefeb3516330a5e
RaldineyR <raldineyr@gmail.com>
2025-11-06 16:07:25 -0300

    fix: add vercel config to resolve dependency conflicts

--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -4,6 +4,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   token: process.env.SANITY_API_TOKEN,
-  useCdn: process.env.NODE_ENV === 'production',
-  apiVersion: '2025-01-01', // Use um formato válido
+  // MUDANÇA CRUCIAL:
+  useCdn: false, // Força a API a não usar o CDN, permitindo a autenticação do token
+  apiVersion: '2025-01-01',
 });
