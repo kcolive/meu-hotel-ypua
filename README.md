1e29c163888de74a447f4d6992c5aa3403d667e1
RaldineyR <raldineyr@gmail.com>
2025-11-06 10:57:10 -0300

    feat: Inicialização do projeto e correções de permissão Sanity 403/500 (Next-Auth e Cadastro Manual) no cliente de escrita.

---
+++ README.md
@@ -0,0 +1,57 @@
+# COMO INICIAR O PROJETO
+
+Pirmeiro, é necessário ter o npm instalado na máquina, aprenda a realizar a instação no link abaixo:
+https://abre.ai/alura-aprenda-a-instalar-o-npm
+
+Segundo, é necessário usar o comando `npm run install` no diretório raiz do projeto — onde há a o arquivo `package.json`.
+
+terceiro, é necessário usar um dos comandos abaixo de acordo com o gerenciador de pacotes que o usuário instalou no computador.
+
+```bash
+`npm run dev`
+# ou
+`yarn dev`
+# or
+`pnpm dev`
+```
+Quarto, abra o seguinte link do localhost padrão: [http://localhost:3000](http://localhost:3000) 
+
+Caso haja um erro semelhante ao informado abaixo é porque o arquivo `.env` não está configurado, favor o configurar ou no caso do SENAI verificar se não foi disponibilizado no AVA especificamente na entrega: AV6 - Prototipar a solução. 
+
+![Captura de tela de 2024-06-29 12-05-12](https://github.com/raldineyr/BACK-END/assets/64384382/4ad201b1-470a-48d6-8c9d-6f8f5cb35fa5)
+
+## EXPORTAR BANCO DE DADOS
+Comando para exportar o banco de dados através do terminal.
+
+```curl https://[ADICIONE-AQUI-O-PROJECTID-DO-SANITY.IO].api.sanity.io/v2021-06-07/data/export/production?types=account,booking,hotelRoom,index,review,user,verificationToken > backup.ndjson```
+
+Exemplo: curl https://sc0ziy2k.api.sanity.io/v2021-06-07/data/export/production?types=account,booking,hotelRoom,index,review,user,verificationToken > backup.ndjson
+
+
+
+## IMAGEM DO PROJETO: CLIENTE
+
+
+![Captura de tela de 2024-06-29 13-16-44](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/75d69223-db00-4d0a-b018-425242b42183)
+![Captura de tela de 2024-06-29 13-16-55](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/83ffff7a-5a19-4815-b429-23ecc07682e6)
+![Captura de tela de 2024-06-29 13-17-03](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/3559b486-dedd-437f-bc2d-1d6b806f2ec3)
+![Captura de tela de 2024-06-29 13-17-28](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/4edec4af-9039-4f90-bf3b-dfa53f69f7df)
+![Captura de tela de 2024-06-29 13-17-40](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/f440d6e5-d258-4188-b381-c6e26123b029)
+![Captura de tela de 2024-06-29 13-17-49](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/55464d05-786e-4e50-8b1a-da9552c06b66)
+
+
+
+## IMAGEM DO PROJETO: ADMNISTRADOR
+
+OBS: SERÁ NECESSÁRIO FAZER UM CADASTRO NO SANITY.io
+
+[http://localhost:3000/studio/structure](http://localhost:3000/studio/structure) 
+
+![Captura de tela de 2024-06-29 13-21-26](https://github.com/raldineyr/algorithms-and-data-structures/assets/64384382/30d31af5-59ef-4167-b81b-3d1e76b1c03d)
+
+
+
+
+
+
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import Link from 'next/link';
+import { useContext } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import ThemeContext from '@/context/themeContext';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+
+const Header = () => {
+    const { darkTheme, setDarkTheme } = useContext(ThemeContext);
+    const { data: session } = useSession();
+    console.log(session);
+
+    const handleScrollToFooter = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        const footerElement = document.querySelector('footer');
+        footerElement?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    return (
+        <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+            <div className="flex items-center w-full md:2/3">
+                <a className="font-black text-tertiary-light" href="https://gerenciamento-hoteleiro.vercel.app/">
+                    <img src="/images/logo-pousada-quinta-do-ypua.png" alt="" className="h-12 md:h-16" />
+                </a>
+            </div>
+            <ul className="flex items-center justify-between w-full md:w1/3 mt-4">
+                <li className='hover:-translate-y-2 duration-500 transition-all'>
+                    <Link href="/">Início</Link>
+                </li>
+                <li className='hover:-translate-y-2 duration-500 transition-all'>
+                    <Link href="/rooms">Acomodações</Link>
+                </li>
+                <li className='hover:-translate-y-2 duration-500 transition-all'>
+                    {/* Adicionando evento para mover ao rodapé */}
+                    <a href="#footer" onClick={handleScrollToFooter}>Contato</a>
+                </li>
+                <ul className='flex items-center ml-5'>
+                    <li className='flex items-center'>
+                        {session?.user ? (
+                            <Link href={`/users/${session.user.id}`}>
+                                {session.user.image ? (
+                                    <div className='w-10 h-10 rounded-full overflow-hidden'>
+                                        <Image
+                                            src={session.user.image}
+                                            alt={session.user.name!}
+                                            width={40}
+                                            height={40}
+                                            className='scale-animation img'
+                                        />
+                                    </div>
+                                ) : (
+                                    <FaUserCircle className='cursor-pointer' />
+                                )}
+                            </Link>
+                        ) : (
+                            <Link href='/auth'>
+                                <FaUserCircle className='cursor-pointer' />
+                            </Link>
+                        )}
+                    </li>
+                    <li className='ml-2'>
+                        {darkTheme ? (
+                            <MdOutlineLightMode
+                                className='cursor-pointer'
+                                onClick={() => {
+                                    setDarkTheme(false);
+                                    localStorage.removeItem('hotel-theme');
+                                }}
+                            />
+                        ) : (
+                            <MdDarkMode
+                                className='cursor-pointer'
+                                onClick={() => {
+                                    setDarkTheme(true);
+                                    localStorage.setItem('hotel-theme', 'true');
+                                }}
+                            />
+                        )}
+                    </li>
+                </ul>
+            </ul>
+        </header>
+    );
+};
+
+export default Header;
